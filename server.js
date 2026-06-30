11cf91a9f7c6c59406ad33501f0884f9810bdbf8
arindamishra <arindamishra07@gmail.com>
2026-06-30 19:29:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const express = require("express");
 const path    = require("path");
 const geminiHandler = require("./api/gemini");
 
+
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
